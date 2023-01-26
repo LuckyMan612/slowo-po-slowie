0c706938901369000cf4dd8bfe061e9a9fd047e0
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-26 21:35:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 function removeWord() {
-  var input = document.getElementById("input").value;
-  var lines = input.split("\n");
-  var newText = "";
-  var wordToRemove = prompt("Enter word to remove:");
-
-  for (var i = 0; i < lines.length; i++) {
-    var words = lines[i].split(wordToRemove);
-    lines[i] = words.join(" ");
-    if (lines[i].endsWith(wordToRemove)) {
-    var words = lines[i].split(wordToRemove);
-    lines[i] = words.join(" ");
-    }
-  }
-
-    for (var j = 0; j < words.length; j++) {
-      if (words[j].indexOf(wordToRemove) !== -1) {
-        var index = words[j].indexOf(wordToRemove) + wordToRemove.length;
-        words[j] = words[j].substring(0, index);
-        break;
-      }
-    }
-
-    newText += words.join(" ") + "<br>";
-  }
-
+  // Pobieranie danych z formularza
+  var text = document.getElementById("textInput").value;
+  var word = document.getElementById("wordToRemove").value;
+  
+  // Usuwanie słowa
+  var newText = text.replace(word, "");
+  
+  // Wyświetlanie wyniku
   document.getElementById("output").innerHTML = newText;
 }
